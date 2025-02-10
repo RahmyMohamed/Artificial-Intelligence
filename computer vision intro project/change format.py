b81a2ebc330f87ecf8ab7f0df00dea2d056d3467
RahmyMohamed <mohamedrahmi428@gmail.com>
2025-02-10 12:00:05 +0530

    Delete computer vision intro project directory

--- computer vision intro project/change format.py
+++
@@ -1,5 +0,0 @@
-import cv2
-
-img=cv2.imread("image1.png")
-cv2.imwrite("sampleimage.jpg" ,img)
-cv2.destroyAllWindows()
--- computer vision intro project/open image.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img= cv2.imread("image1.png")
-
-cv2.imshow("AI_project" ,img)
-cv2.waitKey(100)
-
-cv2.destroyAllWindows()
