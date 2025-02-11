143c108126e40f0b36536d3720e15f381ca45c4f
RahmyMohamed <mohamedrahmi428@gmail.com>
2025-02-11 22:18:30 +0530

    Update 1 project.c

--- C Programing/1 project.c
+++ C Programing/1 project.c
@@ -3,7 +3,6 @@ int main()
 {
 int a=10,b=5,c=20;
 float result;
-//% remainder use pannum bothu bracket kul varum, remainder panne vendiye naum irendum intiger aahe iruthal
 result = (a % b)* c / (2*b);
 
     printf("%f",result);
