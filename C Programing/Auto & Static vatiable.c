c6879691c5f3eabab92a4f8fde9ccc602711cf39
MOHAMED RAHMY <mohamedrahmi428@gmail.com>
2025-02-25 19:42:59 +0530

    Update Auto & Static vatiable.c

--- C Programing/Auto & Static vatiable.c
+++ C Programing/Auto & Static vatiable.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 void fun()
 {
-    static int y = 0;// static fun vanthu aduthu aduthu vare fun ku palaye value vode 1 kooti kondu varum
+    static int y = 0;
     y= y+1;
     printf("y=%d\n",y);
 
-    auto int y = 0;// auto fun narmal fun programing le varvathu
+    auto int y = 0;
     y= y+1;
     printf("y=%d\n",y);
 }
